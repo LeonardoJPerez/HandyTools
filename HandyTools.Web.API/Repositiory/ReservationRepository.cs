a2a349273d7d0c4d4d94d01e64c1f91789fdbaad
Leonardo Perez <find.leonardo@gmail.com>
2016-04-03 01:33:26 -0400

    Additional updates.

--- HandyTools.Web.API/Repositiory/ReservationRepository.cs
+++ HandyTools.Web.API/Repositiory/ReservationRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace HandyTools.Web.API.Repositiory
+﻿using HandyTools.Models;
+using System.Collections.Generic;
+
+namespace HandyTools.Web.API.Repositiory
 {
     public class ReservationRepository : BaseRepository
     {
+        public Reservation GetReservation(int reservationId)
+        {
+            return this._db.GetModel<Reservation>(nameof(reservationId), reservationId.ToString());
+        }
+
+        public List<Reservation> GetReservations(string userName)
+        {
+            // Get reservations from db.
+            // Load up Tools objects.
+            // Load up Handle By Clerk
+            // Load up Picked By clerk.
+
+            return this._db.GetModels<Reservation>(nameof(userName), userName);
+        }
+
+        public Reservation GetReservations(Reservation model)
+        {
+            return this._db.AddModel(model);
+        }
     }
 }
