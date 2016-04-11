b55e26d9894d58954bbc19c9551bbc9185eb958c
Leonardo Perez <find.leonardo@gmail.com>
2016-04-11 10:00:14 -0400

    Added ReportsRepository and API controller. Stubbed out get methods.

---
+++ HandyTools.Models/Enums/ReportType.cs
@@ -0,0 +1,9 @@
+﻿namespace HandyTools.Web.API.Enums
+{
+    public enum ReportType
+    {
+        Report1,
+        Report2,
+        Report3
+    }
+}
---
+++ HandyTools.Web.API/Controllers/ReportsController.cs
@@ -0,0 +1,41 @@
+﻿using HandyTools.Web.API.Enums;
+using HandyTools.Web.API.Interfaces;
+using System.Web.Http;
+
+namespace HandyTools.Web.API.Controllers
+{
+    [RoutePrefix("api/reports")]
+    public class ReportsController : ApiController
+    {
+        protected IReportRepository _repository;
+
+        public ReportsController(IReportRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        // GET api/reports/5
+        [HttpGet]
+        [Route("{type}")]
+        public IHttpActionResult Get(ReportType type)
+        {
+            switch (type)
+            {
+                case ReportType.Report1:
+                    return Ok(this._repository.GetReport1());
+                    break;
+
+                case ReportType.Report2:
+                    return Ok(this._repository.GetReport1());
+                    break;
+
+                case ReportType.Report3:
+                    return Ok(this._repository.GetReport1());
+                    break;
+
+                default:
+                    return BadRequest();
+            }
+        }
+    }
+}
