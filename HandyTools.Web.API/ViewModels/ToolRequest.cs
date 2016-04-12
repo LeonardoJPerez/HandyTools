2bc5527c9a19bc71170248173aa80b6952a563bd
Leonardo Perez <find.leonardo@gmail.com>
2016-04-12 10:43:21 -0400

    Added missing dependency.

---
+++ HandyTools.Web.API/ViewModels/ToolRequest.cs
@@ -0,0 +1,14 @@
+﻿using HandyTools.Web.API.Enums;
+using System;
+
+namespace HandyTools.Web.API.ViewModels
+{
+    public class ToolRequest
+    {
+        public DateTime EndDate { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public ToolType ToolType { get; set; }
+    }
+}
